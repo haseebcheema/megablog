b280f427a6af0af51dfb9ea552d587948daaa95c
haseebcheema <ihaseebtariq1@gmail.com>
2024-04-17 22:43:28 +0400

    added appwrite authentication service

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,5 +1,5 @@
 const conf = {
-    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URLP),
+    appwriteEndPoint: String(import.meta.env.VITE_APPWRITE_URLP),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
