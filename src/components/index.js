cca1f971dabbb6dc995be2ebadbeecd0270f758c
haseebcheema <ihaseebtariq1@gmail.com>
2024-04-19 16:27:34 +0400

    added more components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
 
-export { Header, Footer };  
+export {Container, Logo, Header, LogoutBtn, Footer};  
